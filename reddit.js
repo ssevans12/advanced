701aeea2777f4a05e9d5d9f8eac174cb34b66aad
Sara <ssevans@umich.edu>
2023-03-15 18:26:20 -0400

    added extended challenge and styling

--- reddit.js
+++ reddit.js
@@ -1,26 +1,40 @@
 (function (){
+let posts = [];
+let filteredPosts = [];
 const postsContainer = document.getElementById("posts-container");
 fetch('https://www.reddit.com/r/aww/.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        let posts = data.data.children;
+        posts = data.data.children;
+        filteredPosts = posts;        
+        refreshPosts();
+    })
 
-        for (let post of posts){
-           const postEl = document.createElement("div")
-            postEl.innerText = post.data.title;
-            if (post.data.thumbnail.toLowerCase().includes("https:")){
-                const img = document.createElement("img")
-                img.src = post.data.thumbnail;
-                postEl.append(img);
-            }
-            const link = document.createElement("a");
-            link.href = post.data.url;
-            link.innerText = "link"
-            postEl.append(link);
-            
-            postsContainer.append(postEl);
-        }     
-    }) 
+    const search = document.getElementById("search");
+    search.addEventListener("keyup", (e) => {
+        filteredPosts = posts.filter(p => p.data.title.toLowerCase().includes(e.target.value));
+        refreshPosts();
+    });
+    
+    function refreshPosts(){
+        postsContainer.innerHTML = "";
+        for (let post of filteredPosts){
+            const postEl = document.createElement("div")
+             postEl.innerText = post.data.title;
+             if (post.data.thumbnail.toLowerCase().includes("https:")){
+                 const img = document.createElement("img")
+                 img.src = post.data.thumbnail;
+                 postEl.append(img);
+             }
+             const link = document.createElement("a");
+             link.href = post.data.url;
+             link.innerText = "link"
+             link.target = "_blank"
+             postEl.append(link);
+             
+             postsContainer.append(postEl);
+         }    
+    }
 
 }()); 
